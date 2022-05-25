4740924f5b58d02a1b0c81f4f00c19ffb10b6304
Cibercarmencita <57333041+Cibercarmencita@users.noreply.github.com>
2022-05-25 10:11:32 -0300

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,40 @@
+let cad=`
+<h1></h1>
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.html">Carmen Torrieri</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link" href="biografia.html">Biografía</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="alcubo.html">Al cubo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="gratinando.html">Gratinando</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="novaasersiempreunagomera.html">No va ser siempre una gomera</a>
+      </li>
+      <li class="nav-item">
+         <a class="nav-link" href="enconstruccion.html">Procesos</a>
+      </li>
+      <li class="nav-item">
+         <a class="nav-link" href="contacto.html">Contacto</a>
+      </li>
+    </ul>
+</nav>
+`
+document.getElementById("idheader").innerHTML=cad;
+
+cad=`
+  <a rel="license" href="http://creativecommons.org/licenses/by/2.5/ar/"><img alt="Licencia Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by/2.5/ar/88x31.png" /></a><br />Esta obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by/2.5/ar/">Licencia Creative Commons Atribución 2.5 Argentina</a>.
+    <p> Creative Commons @2022 </p>
+    
+`
+document.getElementById("idfooter").innerHTML=cad;
+
